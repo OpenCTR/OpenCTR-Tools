9415fa0bb27f74a2eae923a001ce334514ae2c06
Alex Shaw <alex.shaw.as@gmail.com>
2015-09-24 16:07:15 -0500

    Remove unneeded version files

--- artool/src/main.c
+++ artool/src/main.c
@@ -1,21 +1,23 @@
 /*
+ *******************************************************************************
  * OpenCTR-Tools - Tools used in the OpenCTR SDK.
- * 
- * Copyright (C) 2015 The OpenCTR Project. 
- * 
- * This file is part of OpenCTR-Tools. 
- * 
- * OpenCTR-Tools is free software: you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License version 3 as 
- * published by the Free Software Foundation.
- * 
- * OpenCTR-Tools is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License 
- * along with OpenCTR-Tools. If not, see <http://www.gnu.org/licenses/>. 
+ *
+ * Copyright (C) 2015 The OpenCTR Project.
+ *
+ * This file is part of OpenCTR-Tools.
+ *
+ * OpenCTR-Tools is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * OpenCTR-Tools is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenCTR-Tools. If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************
  */
 
 #ifdef HAVE_CONFIG_H
